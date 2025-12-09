3584477e0fc2a89053ceac6a7f16bedbec743406
God-yash12 <gannuthapa986@gmail.com>
2025-12-09 19:28:36 +0545

    feat: Initialize new NestJS backend project with package.json and main application file.

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,7 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(AppModule, {
+    logger: ['log', 'error', 'warn', 'debug', 'verbose'],
+  });
+
+  
+
+  
+
+  await app.listen(process.env.PORT || 3000, () => {
+    console.log(`Application is running on: ${app.getUrl()}`);
+  });
 }
 bootstrap();
