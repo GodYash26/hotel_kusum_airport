571988eedf39e895852e8340ca7dbc5f28af88c7
God-yash12 <gannuthapa986@gmail.com>
2025-12-23 12:56:32 +0545

    feat: add Swagger documentation and validation pipes to the NestJS application
    - Integrated Swagger for API documentation with a new endpoint at '/api'.
    - Added global validation pipes to enforce data validation and error handling.
    - Updated CORS settings to allow specific origins.
    - Added new dependencies: @nestjs/config, @nestjs/jwt, @nestjs/swagger, class-transformer, class-validator, multer.
    - Updated pnpm-lock.yaml to reflect new dependencies and their versions.
    - Created pnpm-workspace.yaml to manage workspace dependencies.

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,17 +1,67 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { BadRequestException, ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['log', 'error', 'warn', 'debug', 'verbose'],
   });
 
-  
+  const PORT = process.env.PORT || 3000;
 
-  
+const AllowOrigin = [ "http://localhost:3000", "https://hotel-booking-app-frontend.vercel.app"]
 
-  await app.listen(process.env.PORT || 3000, () => {
-    console.log(`Application is running on: ${app.getUrl()}`);
+  app.enableCors({
+    origin: AllowOrigin,
+    credentials: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    exposedHeaders: 'Content-Length, X-Content-Type-Options',
+    allowedHeaders: 'Content-Type, Authorization',
+    maxAge: 3600,
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  });
+
+
+    app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      stopAtFirstError: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+      validationError: { target: false, value: false },
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+      exceptionFactory: (errors) => {
+        const firstError = errors[0];
+
+        // Make sure constraints exist
+        const constraints = firstError?.constraints;
+        if (!constraints) {
+          return new BadRequestException('Validation failed');
+        }
+
+        const firstConstraint = Object.values(constraints)[0];
+        return new BadRequestException(firstConstraint);
+      },
+    }),
+  );  
+
+  const configswagger = new DocumentBuilder()
+    .setTitle('Hotel Booking API')
+    .setDescription('Hotel Booking API description')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, configswagger);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(PORT, () => {
+    console.log(`Application is running on: http://localhost:${PORT}`);
   });
 }
 bootstrap();
